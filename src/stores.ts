2140578c59197b7126ddf3bdc21b579486ff2858
JEREMY HERCZEG <jeremyherczeg@gmail.com>
2023-11-10 22:13:01 -0700

    make better

--- src/stores.ts
+++ src/stores.ts
@@ -2,6 +2,6 @@ import { writable } from "svelte/store";
 
 export const currentId = writable(0);
 
-export const names = writable(Array);
-export const class_names = writable(Array);
+export const names = writable([]);
+export const class_names = writable([]);
 export const colors = writable(Array(20).fill("#525252"));
