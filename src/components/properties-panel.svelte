9a59ed73ab5a9302a48fc5b6ebf08941ec7bd22f
JEREMY HERCZEG <jeremyherczeg@gmail.com>
2023-11-10 23:09:15 -0700

    vlspti

--- src/components/properties-panel.svelte
+++ src/components/properties-panel.svelte
@@ -13,7 +13,7 @@
     id="properties"
     class="text-white bg-neutral-600 absolute top-5 bottom-5 w-60 right-5 rounded-xl p-3"
   >
-    <h1 class="font-bold text-5xl">{$names[$currentId]}</h1>
+    <h1 class="font-bold capitalize text-5xl">{$names[$currentId]}</h1>
     <h2 class=" text-gray-400">Class: "{$class_names[$currentId]}"</h2>
     <label for="name" class="font-bold">Name: </label>
     <input
