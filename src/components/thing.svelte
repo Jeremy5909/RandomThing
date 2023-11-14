d17ee565df27f388209f93001bc7adb0c8e036e1
JEREMY HERCZEG <jeremyherczeg@gmail.com>
2023-11-14 15:29:53 -0700

    make position a prop

--- src/components/thing.svelte
+++ src/components/thing.svelte
@@ -2,19 +2,12 @@
   import { currentId, colors, names, class_names } from "../stores";
   export let class_name;
   export let id;
+  export let left;
+  export let top;
 
   // Store props
   $names[id] = class_name;
   $class_names[id] = class_name;
-
-  // For random position
-  const minX = 0;
-  const maxX = 400;
-  const minY = 0;
-  const maxY = 400;
-
-  let left = Math.floor(Math.random() * (maxX - minX + 1) + minX);
-  let top = Math.floor(Math.random() * (maxY - minY + 1) + minY);
 
   // Selection
   let border_width = "0";
@@ -50,7 +43,9 @@
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
   on:mousedown={onMouseDown}
-  style=" left: {left}px; top: {top}px; background-color:{$colors[id]}"
+  style=" left: {left - 48}px; top: {top - 48}px; background-color:{$colors[
+    id
+  ]}"
   class="cursor-move rounded-full w-24 h-24 absolute flex select-none justify-center border-{border_width}"
 >
   <img
