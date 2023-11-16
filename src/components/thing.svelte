3b544d25b2431684209761ed95155fc28dce2fee
JEREMY HERCZEG <jeremyherczeg@gmail.com>
2023-11-16 09:50:10 -0700

    move class to stores

--- src/components/thing.svelte
+++ src/components/thing.svelte
@@ -1,36 +1,10 @@
 <script lang="ts">
   import { selectedId } from "../stores";
   export let classs: string;
-  export let name: string;
   export let color: string;
   export let id: number;
   export let left: number;
   export let top: number;
-
-  export class existingThing {
-    itsclass;
-    itsname;
-    itscolor = "525252";
-    itsleft;
-    itstop;
-    selected = false;
-    constructor(
-      itsclass: string,
-      itsname: string,
-      itsleft: number,
-      itstop: number
-    ) {
-      this.itsclass = itsclass;
-      this.itsname = itsname;
-      this.itsleft = itsleft;
-      this.itstop = itstop;
-      console.log("Yess King");
-    }
-  }
-
-  // // Store props
-  // $names[id] = class_name;
-  // $class_names[id] = class_name;
 
   // Selection
   let border_width = "0";
@@ -63,10 +37,12 @@
 
 <svelte:window on:mouseup={onMouseUp} on:mousemove={onMouseMove} />
 
-<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
+  role="treeitem"
+  aria-selected={moving}
+  tabindex="0"
   on:mousedown={onMouseDown}
-  style=" left: {left - 48}px; top: {top - 48}px; background-color:{color}"
+  style="left: {left - 48}px; top: {top - 48}px; background-color:#{color}"
   class="cursor-move rounded-full w-24 h-24 absolute flex select-none justify-center border-{border_width}"
 >
   <img
